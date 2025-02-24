7795cc085ef4401d483e9cc0b6f28ca6d9134bc6
kenrique100 <ngwakenri2016@gmail.com>
2025-02-24 09:35:40 -0500

    removed excess logs

--- expense-service/src/main/java/com/akentech/kbf/expense/controller/ExpenseController.java
+++ expense-service/src/main/java/com/akentech/kbf/expense/controller/ExpenseController.java
@@ -20,42 +20,32 @@ public class ExpenseController {
 
     @GetMapping
     public Flux<Expense> getAllExpenses() {
-        log.info("Received GET request for all expenses");
-        return expenseService.getAllExpenses()
-                .doOnNext(expense -> log.info("Returning expense: {}", expense));
+        return expenseService.getAllExpenses();
     }
 
     @GetMapping("/{id}")
     public Mono<Expense> getExpenseById(@PathVariable String id) {
-        log.info("Received GET request for expense with ID: {}", id);
         return expenseService.getExpenseById(id)
-                .doOnSuccess(expense -> log.info("Returning expense with Id: {}", expense))
                 .doOnError(error -> log.error("Error fetching expense with ID {}: {}", id, error.getMessage()));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Expense> createExpense(@RequestBody @Valid Expense expense) {
-        log.info("Received POST request to create expense: {}", expense);
         return expenseService.createExpense(expense)
-                .doOnSuccess(savedExpense -> log.info("Expense created successfully: {}", savedExpense))
                 .doOnError(error -> log.error("Error creating expense: {}", error.getMessage()));
     }
 
     @PutMapping("/{id}")
     public Mono<Expense> updateExpense(@PathVariable String id, @RequestBody Expense expense) {
-        log.info("Received PUT request to update expense with ID: {}", id);
         return expenseService.updateExpense(id, expense)
-                .doOnSuccess(updatedExpense -> log.info("Expense updated successfully: {}", updatedExpense))
                 .doOnError(error -> log.error("Error updating expense with ID {}: {}", id, error.getMessage()));
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> deleteExpense(@PathVariable String id) {
-        log.info("Received DELETE request for expense with ID: {}", id);
         return expenseService.deleteExpense(id)
-                .doOnSuccess(v -> log.info("Expense with ID {} deleted successfully", id))
                 .doOnError(error -> log.error("Error deleting expense with ID {}: {}", id, error.getMessage()));
     }
 }

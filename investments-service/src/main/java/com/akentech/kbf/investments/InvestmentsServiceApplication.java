0c327bd352af27b0f13fbffd17e6c7d9bbac7f10
kenrique100 <ngwakenri2016@gmail.com>
2025-02-24 05:22:00 -0500

    updated commit

---
+++ investments-service/src/main/java/com/akentech/kbf/investments/InvestmentsServiceApplication.java
@@ -0,0 +1,11 @@
+package com.akentech.kbf.investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentsServiceApplication.class, args);
+    }
+}

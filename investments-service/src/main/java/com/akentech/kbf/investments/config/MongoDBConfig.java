969fd2053fccfee8db8aabe80658e051eeacb563
kenrique100 <ngwakenri2016@gmail.com>
2025-02-24 06:34:04 -0500

    mongoDb connection setup

---
+++ investments-service/src/main/java/com/akentech/kbf/investments/config/MongoDBConfig.java
@@ -0,0 +1,9 @@
+package com.akentech.kbf.investments.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+
+@Configuration
+@EnableReactiveMongoAuditing
+public class MongoDBConfig {
+}

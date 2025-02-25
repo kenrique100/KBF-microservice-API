5565bf555a033cda67419fe37bc51883e6a37fec
kenrique100 <ngwakenri2016@gmail.com>
2025-02-25 11:11:09 -0500

    removed used function

--- transaction-service/src/main/java/com.akentech.kbf.transaction/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com.akentech.kbf.transaction/TransactionServiceApplication.java
@@ -2,10 +2,7 @@ package com.akentech.kbf.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
 @SpringBootApplication
-@EnableMongoRepositories
 public class TransactionServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransactionServiceApplication.class, args);
